fae8b55f6bfafde4c23589130732c53bacc0a339
Christine Rosa <rosa0072@algonquinlive.com>
2021-05-02 20:10:37 -0400

    Creates react app, consumes API, and displays movie results (title, year, & poster) based on search.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  // States
+  const [searchResults, setSearchResults] = useState([]);
+
+
+  // API Requests
+  const getData = async (searchValue) => {
+    console.log(searchValue);
+    let data = await axios.get(`http://www.omdbapi.com/?apikey=84d86ab5&type=movie&s=${searchValue}`);
+    setSearchResults(data.data.Search);
+    console.log(searchResults);
+  }
+
+  // Handles
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      getData(e.target.value);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Shoppies</h1>
+      <div className='search'>
+        <label>Movie Title</label><br></br>
+        <input type="text" onKeyDown={handleSearch}/>
+      </div>
+      <div>
+        {
+          !searchResults ? 
+          <p>No Results Found.</p> :
+          searchResults.map((item, index) => {
+            return (
+                <div key={index}>
+                  <img src={item.Poster} alt=''/>
+                  <h2>{item.Title}</h2>
+                  <h4>{item.Year}</h4>
+                </div>
+              )
+          })
+        }
+      </div>
     </div>
+    
   );
 }
 
 export default App;
+
+// Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+// Title: "The Lord of the Rings: The Return of the King"
+// Type: "movie"
+// Year: "2003"
+// imdbID: "tt0167260"
